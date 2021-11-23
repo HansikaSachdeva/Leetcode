551d20ec827cc81231415a4fb9eef93bc754a830
Hansika Sachdeva <52819652+HansikaSachdeva@users.noreply.github.com>
2021-11-24 01:38:21 +0530

    Update add-digits.cpp

--- add-digits/add-digits.cpp
+++ add-digits/add-digits.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int addDigits(int num) {
+        
+        int temp = num;
+        while(temp/10>0){
+            int val = temp;
+            int sum = 0;
+            while(val>0){
+                sum += val%10;
+                val /=10;
+            }
+            temp=sum;
+        }
+        return temp;
+        
+        
+        
+        
+        /* Recursion
         if(num/10==0) return num;
         int sum = 0;
         while(num>0){
@@ -8,5 +25,6 @@ public:
             num/=10;
         }
         return addDigits(sum);
+        */
     }
 };
